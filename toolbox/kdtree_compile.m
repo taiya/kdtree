51996679509d541fc3cc2d27d5b9bfc0b433f56d
Andrea <andrea.tagliasacchi@gmail.com>
2012-06-07 11:22:37 -0700

    Added windows binaries Fixed missing compile steps in .m file

--- toolbox/kdtree_compile.m
+++ toolbox/kdtree_compile.m
@@ -10,6 +10,8 @@ err = err | mex('-outdir',localpath, [localpath,'/kdtree_k_nearest_neighbors.cpp
 err = err | mex('-outdir',localpath, [localpath,'/kdtree_ball_query.cpp']);
 err = err | mex('-outdir',localpath, [localpath,'/kdtree_nearest_neighbor.cpp']);
 err = err | mex('-outdir',localpath, [localpath,'/kdtree_range_query.cpp']);
+err = err | mex('-outdir',localpath, [localpath,'/kdtree_io_from_mat.cpp']);
+err = err | mex('-outdir',localpath, [localpath,'/kdtree_io_to_mat.cpp']);
 
 if err ~= 0, 
    error('compile failed!'); 
